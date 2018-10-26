4773d9e9588f431744641ba0dc12fec0ff20879d
JoseJota <jose.julian.perez.lucas@gmail.com>
2018-10-26 12:22:41 +0200

    Pequenios cambios

--- CuentaPalabras/CuentaPalabras.php
+++ CuentaPalabras/CuentaPalabras.php
@@ -1,7 +1,6 @@
 <?php
 
 $fichero=[];
-
 if(isset($_FILES) && isset($_FILES['fichero'])){
     $ruta=$_FILES['fichero']['tmp_name']; //ruta temporal donde está el fichero
     //echo file_get_contents($ruta);
@@ -9,22 +8,38 @@ if(isset($_FILES) && isset($_FILES['fichero'])){
     move_uploaded_file($ruta, 'ficheros/'.$nombre);
 
     $contenidoString=file_get_contents('ficheros/'.$nombre);
-    $contenidoArray=file('ficheros/'.$nombre);
+    //$contenidoArray=file('ficheros/'.$nombre);
 
-echo str_word_count($contenidoString);
+//print_r(str_word_count($contenidoString, 1, 'àáãç3'));
 
-print_r(str_word_count($contenidoString, 1, 'àáãç3'));
+$todasPalabras = str_word_count($contenidoString, 1, 'àáãç3');
 
-/* var_dump($contenidoArray);
 
-explode(" ", $contenidoArray);  
 
-var_dump($contenidoArray);
- */
-//USAR EXPLODE PARA PASAR DE UN STRING A UN ARRAY CON CADA PALABRA
+$frecuencia = []; 
+
+foreach($todasPalabras as $palabras){
+    if(isset($frecuencia[$palabras])){
+        $frecuencia[$palabras]++;
+    }else{
+        $frecuencia[$palabras]=1;
+    }
 }
-
 ?>
+
+<table border=1><tr><th>Palabra</th><th>Frecuencia</th></tr>
+
+<?php
+
+
+
+foreach($frecuencia as $palabras=>$frecuencia){
+    echo "<tr><td>".$palabras."</td>";
+    echo "<td>".$frecuencia."</td></tr>";
+}
+}
+?>
+
 <html>
 <head>
     <meta charset="utf-8" />
